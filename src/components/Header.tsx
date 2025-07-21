0d44b7f48aaf88501cba438d891f2b3f9d21e738
Vidisha231106 <vidishadewan2020@gmail.com>
2025-07-21 20:04:21 +0530

    Changed the Header.tsx and Footer.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import Button from './Button';
-import RVCELogoUrl from '/logos/RVCE_Logo.png';
 
 const RVCELogo: React.FC = () => (
     <img 
-        src={RVCELogoUrl} 
+        src="/logos/RVCE_Logo.png" 
         alt="RVCE Logo" 
         className="w-10 h-10 object-contain"
         style={{ filter: 'brightness(0)' }}
